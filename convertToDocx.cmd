dd0951a6bd75043acf23f04ab61482cb6e1c7480
Surjit Kumar Sahoo <surjitsahoo0@gmail.com>
2022-03-01 23:11:47 +0530

    re-organised files

--- convertToDocx.cmd
+++
@@ -1,3 +0,0 @@
-@REM ^ is used for new line
-asciidoctor --backend docbook --out-file - participant3.adoc | ^
-pandoc --from docbook --to docx --output build/participant3.docx
--- converter.js
+++ converter.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 docbookConverter.register();
 
-const SOURCE_DIR = './';
+const SOURCE_DIR = './content/';
 
 function pandocCallback(err, result, file) {
 	if (err) {
@@ -20,7 +20,7 @@ function convert() {
 			if (file.endsWith('.adoc')) {
 				try {
 					// read the file
-					const content = fs.readFileSync(file, 'utf8');
+					const content = fs.readFileSync(SOURCE_DIR + file, 'utf8');
 					// convert to docbook
 					const docbook = asciiDoctor.convert(content, { backend: 'docbook5' });
 					// check if the build directory exists, if not create the directory
